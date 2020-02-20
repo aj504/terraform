86900548bba83935efe06df7589258da5d5ccf0b
johnsoan3admin <anthony.johnson@globalpay.com>
2020-02-20 13:54:33 -0800

    updating bucket names

--- exercise_3/main.tf
+++ exercise_3/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 terraform {
    backend "s3" {
-   bucket = "rc-tf-remote-state-bucket"
+   bucket = "aj-tf-bucket-training-exercise-1"
    region = "us-east-1"
   }
 }
@@ -15,11 +15,11 @@ resource "aws_s3_bucket" "tf-root-module-bucket" {
   bucket = "${var.s3_bucket_name}"
   acl = "private"
   region = "${var.s3_bucket_region}"
-  
+
   versioning {
     enabled = true
   }
-  
+
   tags = {
     Name        = "${var.s3_bucket_name}"
     Environment = "${var.tag_env}"
